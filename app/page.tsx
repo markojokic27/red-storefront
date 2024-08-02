8ff2dc3fb5b0dc7ed11c3a816cf343c0911d497b
markojokic27 <jokicmarko98@gmail.com>
2024-08-02 15:34:37 +0200

    icons added

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,25 @@ import Button from "@/components/Button";
 export default function Home() {
   return (
     <main className="flex column gap-4 p-6">
-      <Button variant="solid" size="md">Label</Button>
-      <Button variant="outline" size="sm">Label</Button>
-      <Button variant="solid" colorScheme="gray" size="md">Label</Button>
-      <Button variant="solid" colorScheme="blue" size="sm" disabled>Label</Button>
-      <Button variant="outline" colorScheme="blue" size="md" disabled>Label</Button>
-      <Button variant="solid" colorScheme="gray" size="sm" disabled>Label</Button>
-
+      <Button variant="solid" size="md">
+        Label
+      </Button>
+      <Button variant="outline" size="sm">
+        Label
+      </Button>
+      <Button variant="solid" colorScheme="gray" size="md">
+        Label
+      </Button>
+      <Button variant="solid" colorScheme="blue" size="sm" disabled>
+        Label
+      </Button>
+      <Button variant="outline" colorScheme="blue" size="md" disabled>
+        Label
+      </Button>
+      <Button variant="solid" colorScheme="gray" size="sm" disabled>
+        Label
+      </Button>
+      
     </main>
   );
 }
